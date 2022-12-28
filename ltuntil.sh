60d3ba4c2b18a6e6c7c9dff8163cc81078ea8d3d
Balla Vamsi Srinivas <ballavamsisrinivas@gmail.com>
2022-12-28 13:29:27 +0530

    Added logs

--- ltuntil.sh
+++ ltuntil.sh
@@ -1,14 +1,18 @@
 final_command="lt"
 if [[ -n "$LT_LOCALHOST" ]]; then
+  echo "setting --localhost=$LT_LOCALHOST"
   final_command="$final_command --local-host=$LT_LOCALHOST"
 fi
 if [[ -n "$LT_PORT" ]]; then
+  echo "setting --port=$LT_PORT"
   final_command="$final_command --port=$LT_PORT"
 fi
 if [[ -n "$LT_SUBDOMAIN" ]]; then
+  echo "setting --subdomain=$LT_SUBDOMAIN"
   final_command="$final_command --subdomain=$LT_SUBDOMAIN"
 fi
 if [[ -n "$LT_OTHER_ARGS" ]]; then
+  echo "setting other args $LT_OTHER_ARGS"
   final_command="$final_command $LT_OTHER_ARGS"
 fi
 
