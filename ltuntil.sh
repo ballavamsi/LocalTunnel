058780d746e83b7a9e69fb5abcc87ca2b683b65b
Balla Vamsi Srinivas <ballavamsisrinivas@gmail.com>
2022-12-28 13:26:45 +0530

    Fixed dos2unix

--- ltuntil.sh
+++ ltuntil.sh
@@ -1,18 +1,14 @@
 final_command="lt"
 if [[ -n "$LT_LOCALHOST" ]]; then
-  echo "--local-host is set to $LT_LOCALHOST"
   final_command="$final_command --local-host=$LT_LOCALHOST"
 fi
 if [[ -n "$LT_PORT" ]]; then
-  echo "--port is set to $LT_PORT"
   final_command="$final_command --port=$LT_PORT"
 fi
 if [[ -n "$LT_SUBDOMAIN" ]]; then
-  echo "--subdomain is set to $LT_SUBDOMAIN"
   final_command="$final_command --subdomain=$LT_SUBDOMAIN"
 fi
 if [[ -n "$LT_OTHER_ARGS" ]]; then
-  echo "Other arguments are set to $LT_OTHER_ARGS"
   final_command="$final_command $LT_OTHER_ARGS"
 fi
 
